8ce44afc78c6a533215b328f69037061c67f2873
Martin Mauch <martin.mauch@gmail.com>
2017-05-15 16:31:38 +0200

    Minified even more

--- src/test/scala/spark/DataFrameConversionsSpec.scala
+++ src/test/scala/spark/DataFrameConversionsSpec.scala
@@ -1,15 +1,11 @@
 package spark
 
-import DataFrameConversions._
-
-import java.time.LocalDate
-import org.apache.spark.sql.types.StructField
-import org.scalatest.{EitherValues, FunSpec, Matchers, BeforeAndAfter}
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql._
-import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.types.{StructType, DataTypes}
-import scala.reflect.runtime.universe._
-import scala.collection.mutable.LinkedHashMap
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+import org.scalatest.{FunSpec, Matchers}
 
 case class Outermost[+T](
   obj: T,
@@ -19,48 +15,40 @@ case class Middle[T](
   scope: T
 )
 case class Inner(
-  id: Long
+  id: Int
 )
-class DataFrameConversionsSpec
-  extends FunSpec
-    with Matchers
-{
+
+class DataFrameConversionsSpec extends FunSpec with Matchers {
+
   val spark = SparkSession
     .builder()
     .master(s"local[2]")
     .getOrCreate()
-  implicit val sparkImplicits = spark.implicits
-  implicit val sparkConfig = spark.sparkContext.getConf
-  implicit val sqlContext = spark.sqlContext
-  import sparkImplicits._
+  import spark.implicits._
 
   val flatSchema = Seq(col("obj.scope.id"), col("someValue"))
 
   def toFlatDataFrame(ts: Seq[Outermost[Middle[Inner]]]): DataFrame =
     spark.createDataset[Outermost[Middle[Inner]]](ts).toDF.select(flatSchema: _*)
 
-  val columnSpec = StructColumn("",Vector(StructColumn("obj",Vector(StructColumn("scope",Vector(PrimitiveColumn("id",typeOf[Long],None))))), PrimitiveColumn("someValue",typeOf[Double],None)))
-  def doBusyWork(): Unit = {
-    for(i <- 0 to 10) {
-      val flatDf = toFlatDataFrame(Seq.empty[Outermost[Middle[Inner]]]).cache
-      val dataset = flatDf.toDsFromSpec[Outermost[Middle[Inner]]](columnSpec)
-      dataset.collect
-    }
+  val rows = List(Outermost(Middle(Inner(1)),1.0), Outermost(Middle(Inner(2)),2.0))
+  val innerSchema = StructType(Seq(StructField("id",IntegerType,true)))
+  val middleSchema = StructType(Seq(StructField("scope",innerSchema,true)))
+
+  def unflattenDf(df: DataFrame): Seq[Outermost[Middle[Inner]]] = {
+    val dataset = df.withColumn("obj", struct(struct(col("id"))).cast(middleSchema)).as[Outermost[Middle[Inner]]]
+    dataset.collect
   }
 
-  val rows = List(Outermost(Middle(Inner(-262085534340088811L)),-1.932810953673972E247), Outermost(Middle(Inner(3975961295672879120L)),3.0437389599087003E-48))
-
   it("failing example without cache") {
-    doBusyWork()
-    val flatDf = toFlatDataFrame(rows)
-    val dataset = flatDf.toDsFromSpec[Outermost[Middle[Inner]]](columnSpec)
-    dataset.collect should equal(rows)
+    unflattenDf(toFlatDataFrame(rows)) should equal(rows)
   }
 
   it("working example with cache") {
-    doBusyWork()
-    val flatDf = toFlatDataFrame(rows).cache
-    val dataset = flatDf.toDsFromSpec[Outermost[Middle[Inner]]](columnSpec)
-    dataset.collect should equal(rows)
+    unflattenDf(toFlatDataFrame(rows).cache) should equal(rows)
+  }
+
+  it("same as the failing example, but this time it works") {
+    unflattenDf(toFlatDataFrame(rows)) should equal(rows)
   }
 }
